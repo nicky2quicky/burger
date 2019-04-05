a12c267978c9a161aeb5fbcb571940ef3d9008e8
nicky2quicky <NICKTOM22@GMAIL.COM>
2019-04-04 20:52:42 -0700

    created the DBs

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name, eaten) VALUES ('Krusty Burger', false);
+INSERT INTO cats (name, eaten) VALUES ('Bobs Burger', false);
+INSERT INTO cats (name, eaten) VALUES ('Honker Burger', false);
+INSERT INTO cats (name, eaten) VALUES ('McDowells Burger', false);
+INSERT INTO cats (name, eaten) VALUES ('Shakey Burger', false);
