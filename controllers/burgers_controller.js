2578b9434c8d6b7474edb71f2ec4fd25f3b5e0ec
nicky2quicky <NICKTOM22@GMAIL.COM>
2019-04-05 17:02:06 -0700

    burger controller js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+// Setting up Requirements and Express
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
